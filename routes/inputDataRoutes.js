8fab611d198cf0eab110b120e513335bfb1a7543
faldymartinus <faldymartinus@gmail.com>
2022-05-23 18:52:58 +0700

    Change storage to cloud-storage

--- routes/inputDataRoutes.js
+++ routes/inputDataRoutes.js
@@ -9,6 +9,9 @@ const { json } = require('express/lib/response');
 const db = require('../server')
 const _ = require('underscore')
 const { isMapIterator } = require('util/types')
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+const https= require('https');
 
 app.use(bodyParser.json())
 
@@ -26,125 +29,99 @@ app.get('/:tempatParkir&:floor&:cluster&:slot', async (req, res) => {
   res.send(Object.values(result[0].denah[0])).status(201)
 })
 
-app.get('/input', (req, res) => {
-  inputDataFromJson()
+app.get('/cloud/:filename', async(req,res)=>{
+  const filename = req.params.filename
+  readFromCloudStorage(filename)
+  res.send('halo')
+});
+
+async function readFromCloudStorage(filename){
+  bucketName = 'carkir-storage'
+  const nameExtension = filename + '.json'
+  const contents = await storage.bucket(bucketName).file(nameExtension).download();
+    let floor = 1
+    const data = JSON.parse(contents)
+    const result = _.countBy(data, function (data1) {
+      if (data1.Floor == floor){
+        if (data1.Occupancy == 1.0) {
+          let i = data1.Floor + "" + data1.Cluster
+          return i;
+        }
+      } else {
+        floor = data1.floor
+        if (data1.Occupancy == 1.0){
+          let i = data1.Floor + "" + data1.Cluster
+          return i;
+        }
+      }
+    })
+  await Item.updateOne({ tempatParkir: filename },
+    { $set: {
+        clusterCount: result
+      }
+    })
+}
+
+app.get('/input/:filename', (req, res) => {
+  const filename = req.params.filename
+  inputDataFromJson(filename)
+  countCluster(filename)
   res.send('halo')
 })
 
-app.get('/update', async (req, res) => {
-  updateDataFromJson()
-  res.send("update berhasil")
-})
+async function inputDataFromJson(filename) {
+  bucketName = 'carkir-storage'
+  const nameExtension = filename + '.json'
+  const contents = await storage.bucket(bucketName).file(nameExtension).download();
 
-async function inputDataFromJson() {
-  const directoryPath = path.join(__dirname, '../Documents');
-  //passsing directoryPath and callback function
-  fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    files.forEach(async function (file) {
-      const data = fs.readFileSync(path.resolve(__dirname, "../Documents/" + file), "utf8", (err, jsonString) => {
-        if (err) {
-          console.log("File read failed:", err);
-          return;
+  const result = await Item.findOne({
+    tempatParkir: `${filename}`
+  })
+
+  if(result){
+    await Item.updateOne(
+      { tempatParkir: filename },
+      { $set: {
+          denah: JSON.parse(contents)
         }
-      });
-      const item = new Item({
-        tempatParkir: path.parse(file).name,
-        headerImage: null,
-        alamat: null,
-        denah: JSON.parse(data)
-      })
-      await item.save()
-      console.log(file);
-    });
-  });
-}
+      }
+    )
+    return
+  }
 
-async function updateDataFromJson() {
-  const directoryPath = path.join(__dirname, '../Documents');
-  //passsing directoryPath and callback function
-  fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    files.forEach(async function (file) {
-      const data = fs.readFileSync(path.resolve(__dirname, "../Documents/" + file), (err, jsonString) => {
-        if (err) {
-          console.log("File read failed:", err);
-          return;
-        }
-      });
-      await Item.updateOne({ tempatParkir: path.parse(file).name },
-        {
-          $set: {
-            denah: JSON.parse(data)
-
-          }
-        })
-      console.log(file);
-    });
-  });
+  const item = new Item({
+    tempatParkir: filename,
+    headerImage: null,
+    alamat: null,
+    denah: JSON.parse(contents)
+  })
+  await item.save()
 }
 
 async function countCluster() {
-  const directoryPath = path.join(__dirname, '../Documents');
-  //passsing directoryPath and callback function
-  const result = fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    const result = ''
-    files.forEach(async function (file) {
-      const dataRaw = fs.readFileSync(path.resolve(__dirname, "../Documents/" + file), (err, jsonString) => {
-        if (err) {
-          console.log("File read failed:", err);
-          return;
+  bucketName = 'carkir-storage'
+  const nameExtension = filename + '.json'
+  const contents = await storage.bucket(bucketName).file(nameExtension).download();
+    let floor = 1
+    const data = JSON.parse(contents)
+    const result = _.countBy(data, function (data1) {
+      if (data1.Floor == floor){
+        if (data1.Occupancy == 1.0) {
+          let i = data1.Floor + "" + data1.Cluster
+          return i;
         }
-      });
-      let floor = 1
-      const data = JSON.parse(dataRaw)
-      const result = _.countBy(data, function (data1) {
-        if (data1.Floor == floor) {
-          if (data1.Occupancy == 1.0) {
-            let i = data1.Floor + "" + data1.Cluster
-            return i;
-          }
-        } else {
-          floor = data1.floor
-          if (data1.Occupancy == 1.0) {
-            let i = data1.Floor + "" + data1.Cluster
-            return i;
-          }
+      } else {
+        floor = data1.floor
+        if (data1.Occupancy == 1.0){
+          let i = data1.Floor + "" + data1.Cluster
+          return i;
         }
-      })
-      await Item.updateOne({ tempatParkir: path.parse(file).name },
-        {
-          $set: {
-            clusterCount: result
-          }
-        })
-    });    
-  });
+      }
+    })
+  await Item.updateOne({ tempatParkir: filename },
+    { $set: {
+        clusterCount: result
+      }
+    })
 }
-
-var fsTimeout
-
-fs.watch('Documents', function (e) {
-
-  if (!fsTimeout) {
-    updateDataFromJson()
-    countCluster()
-    fsTimeout = setTimeout(function () { fsTimeout = null }, 5000) // give 5 seconds for multiple events
-  }
-})
-
-app.post('/register2',)
 module.exports = app
