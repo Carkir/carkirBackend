f7297d8d2d5fffc558ca2bd9e86a203696e45c49
unknown <yusuf.agung@ui.ac.id>
2022-05-26 17:39:25 +0700

    minor dockerfile revision

--- functions/tokenize.js
+++ functions/tokenize.js
@@ -6,7 +6,7 @@ function verifyToken(req, res, next){
     const autHeader = req.headers['authorization']
     const token = autHeader && autHeader.split(' ')[1]
     if(token == null) return res.sendStatus(403)
-    jwt.verify(token,process.env.SECRET_KEY,(err,user)=>{
+    jwt.verify(token,`${process.env.SECRET_KEY}`,(err,user)=>{
         if(err) return res.sendStatus(401)
         req.user = user
         next()
