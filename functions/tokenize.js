15b75d1e9b8f1b7a7ef9b5f4994d92c3e91d7073
unknown <yusuf.agung@ui.ac.id>
2022-05-26 16:59:34 +0700

    Add Token Support for ML --> Storage path

---
+++ functions/tokenize.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+function createToken(){}
+
+function verifyToken(req, res, next){
+    const autHeader = req.headers['authorization']
+    const token = autHeader && autHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(403)
+    jwt.verify(token,process.env.SECRET_KEY,(err,user)=>{
+        if(err) return res.sendStatus(401)
+        req.user = user
+        next()
+    })
+
+}
+
+module.exports = {verifyToken}
--- tes.js
+++
@@ -1,5 +0,0 @@
-const validator = require('email-validator')
-const regex = new RegExp('^[A-Za-z][A-Za-z0-9]*$')
-const str = 'diltonsa '
-console.log(regex.test(str))
-console.log(validator.validate('yusuf.alazhar@gmail.com'))
